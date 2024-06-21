8c202d42608af4d41d7b9e85f0b8fcaec62aaff0
Lucas Mateus <Lucas015x@gmail.com>
2024-06-21 15:09:20 -0300

    feat: removendo a tabela comments

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91920274d9d639e59e499dbaa1938d2ebb88bd80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+927e9c335fc7f78b2e8d544c3c6d011ab8161904")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
