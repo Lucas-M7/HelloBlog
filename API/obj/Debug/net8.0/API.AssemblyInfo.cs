74bb9431b929fb701e95e069a65ad9461d3258b0
Lucas Mateus <Lucas015x@gmail.com>
2024-06-21 13:23:55 -0300

    feat: configurando a conexao com o banco de dados

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca5a6b5645844c5858bf4b747fb4d2ff4a7929da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c16f27cf8652cddac7217a68b3730a077b2ae3f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
