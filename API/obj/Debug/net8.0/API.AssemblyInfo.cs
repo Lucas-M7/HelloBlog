5b45e60e92a6ef78b85e489abcb1dab8a5b2c377
Lucas Mateus <Lucas015x@gmail.com>
2024-06-21 15:19:40 -0300

    feat: modificando o gitignore

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d181fafdaf6f6143a5676cab7d0621343c2269d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33ac26d8ec3ac209bad62d44cbb77fc9e6a52c66")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
